9cc2a24528cc286b49d0135fd6ebf4d93428e394
JOHN Li <zhuang.li@ibm.com>
2018-09-18 12:29:01 -0400

    Tests and implementation

--- board.py
+++ board.py
@@ -3,25 +3,62 @@ from enum import Enum
 import random
 import numpy as np
 
-
-class eCellState(Enum):
-    UNDEFINED=0
+class eCellState:
     ALIVE=1
-    DEAD=2
+    DEAD=0
 
 
 class Board:
-    board = None   
+    board = None
+    x = None
+    y = None   
     def generate(self,x,y):
-        self.board = np.random.random_integers(0,3,(x,y))
+        self.board = np.random.random_integers(0,2,(x,y))
+        self.x = x
+        self.y = y
     def setBoard(self,x): 
         self.board = np.array(x)
+        self.x = self.board.shape[0]
+        self.y = self.board.shape[1]
     def nextGen(self):
+        #TODO:
         pass
+    def getNextState(self, x, y):
+        AliveNei = self.getNumOfAliveNeighbor(x, y)
+        if self.board[(x,y)] == eCellState.ALIVE:
+            if AliveNei < 2:
+                return eCellState.DEAD
+            elif AliveNei == 2 or AliveNei == 3:
+                return eCellState.ALIVE
+            elif AliveNei > 3:
+                return eCellState.DEAD
+        else:
+            if AliveNei == 3:
+                return eCellState.ALIVE
+            else:
+                return eCellState.DEAD
+
     def getNumOfAliveNeighbor(self,x,y):
-        pass
-    def getNumOfDeadNeighbor(self,x,y):
-        pass
+        # Wrapped board, need test
+        tl = ((x + self.x - 1)%self.x,(y + self.y - 1)%self.y)
+        ml = ((x + self.x - 1)%self.x,y)
+        bl = ((x + self.x - 1)%self.x,(y + self.y + 1)%self.y)
+        tm = (x,(y + self.y - 1)%self.y)
+        bm = (x,(y + self.y + 1)%self.y)
+        tr = ((x + self.x + 1)%self.x,(y + self.y - 1)%self.y)
+        mr = ((x + self.x + 1)%self.x,y)
+        br = ((x + self.x + 1)%self.x,(y + self.y + 1)%self.y)
+        alive_count = 0
+        alive_count += 1 if self.board[tl] == eCellState.ALIVE else 0
+        alive_count += 1 if self.board[ml] == eCellState.ALIVE else 0
+        alive_count += 1 if self.board[bl] == eCellState.ALIVE else 0
+        alive_count += 1 if self.board[tm] == eCellState.ALIVE else 0
+        alive_count += 1 if self.board[bm] == eCellState.ALIVE else 0
+        alive_count += 1 if self.board[tr] == eCellState.ALIVE else 0
+        alive_count += 1 if self.board[mr] == eCellState.ALIVE else 0
+        alive_count += 1 if self.board[br] == eCellState.ALIVE else 0
+        return alive_count
+
     def printBoard(self):
         print(self.board)
 
--- test/test_board.py
+++ test/test_board.py
@@ -4,9 +4,37 @@ import numpy as np
 class test_board(unittest.TestCase):
     def setUp(self):
         self.board = Board()
-        self.board.generate(5,5)
+        self.board.setBoard([[1,1,1],[1,1,1],[1,1,1]])
+    def test_getElement(self):
+        self.assertEqual(self.board.board[(0,0)],1)
     def test_initialize(self):
         self.assertIsInstance(self.board.board,type(np.array([1,2,3])))
-        
+    def test_getAliveNeighbor(self):
+        self.assertEqual(self.board.getNumOfAliveNeighbor(1,1),8)
+
+class test_senarios(unittest.TestCase):
+    def test_UnderPopulation(self):
+        a = Board()
+        a.setBoard([[0,0,0],[0,1,0],[0,0,0]])
+        self.assertEqual(0,a.getNextState(1,1))
+    def test_Surive2(self):
+        a = Board()
+        a.setBoard([[1,1,0],[0,1,0],[0,0,0]])
+        self.assertEqual(1,a.getNextState(1,1))
+        pass
+    def test_Surive3(self):
+        a = Board()
+        a.setBoard([[1,1,1],[0,1,0],[0,0,0]])
+        self.assertEqual(1,a.getNextState(1,1))
+        pass
+    def test_OverCrowding(self):
+        a = Board()
+        a.setBoard([[1,1,1],[0,1,0],[1,0,0]])
+        self.assertEqual(0,a.getNextState(1,1))
+    def test_Reproduction(self):
+        a = Board()
+        a.setBoard([[1,1,1],[0,0,0],[0,0,0]])
+        self.assertEqual(1,a.getNextState(1,1))
+        pass
 if __name__ == "__main__":
     unittest.main()
