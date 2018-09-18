3b71adf01c5b9f55f0d6eebb18f417ccb31bf713
JOHN Li <zhuang.li@ibm.com>
2018-09-18 10:42:53 -0400

    fix test

--- test/test_board.py
+++ test/test_board.py
@@ -4,9 +4,9 @@ import numpy as np
 class test_board(unittest.TestCase):
     def setUp(self):
         self.board = Board()
+        self.board.generate(5,5)
     def test_initialize(self):
-        self.assertIsInstance(self.board,np.array)
-
-
+        self.assertIsInstance(self.board.board,type(np.array([1,2,3])))
+        
 if __name__ == "__main__":
     unittest.main()
